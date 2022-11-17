c2d40d7cdafaf51624aad7ec951dbc95f67104c0
fenris <fenristhefox@gmail.com>
2022-11-17 00:31:08 -0600

    reformatting

---
+++ fencDecode.h
@@ -0,0 +1,14 @@
+//
+// Created by fenris on 11/17/22.
+//
+
+#ifndef FENC_FENCDECODE_H
+#define FENC_FENCDECODE_H
+
+
+class fencDecode {
+
+};
+
+
+#endif //FENC_FENCDECODE_H
---
+++ fencEncode.h
@@ -0,0 +1,16 @@
+//
+// Created by fenris on 11/17/22.
+//
+
+#ifndef FENC_FENCENCODE_H
+#define FENC_FENCENCODE_H
+
+
+class fencEncode {
+    char returnTest(char i) {
+        return i;
+    }
+};
+
+
+#endif //FENC_FENCENCODE_H
