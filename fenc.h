4f164cdebe616837c055d58511a67bde9939437c
fenris <fenristhefox@gmail.com>
2022-11-17 01:01:35 -0600

    reformatting

--- fenc.h
+++ fenc.h
@@ -2,15 +2,25 @@
 // Created by fenris on 11/17/22.
 //
 
-#ifndef FENC_FENCDECODE_H
-#define FENC_FENCDECODE_H
+#ifndef FENC_FENC_H
+#define FENC_FENC_H
 
 
 class fenc {
-    public char test(char t) {
+    mutable table[] = {
+            [0, 'a'],[1, 'b'],[2, 'c'],[3, 'd'],[4, 'e'],
+            [5, 'f'],[6, 'g'],[7, 'h'],[8, 'i'],[9, 'j'],
+            [10, 'k'],[11, 'l'],[12, 'm']
+    }
+
+public: char test(char t) {
         return t;
     }
+
+
+
+
 };
 
 
-#endif //FENC_FENCDECODE_H
+#endif //FENC_FENC_H
