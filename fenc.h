2a2ffacd6f4e0e7bdb9b237c29c73a88a1759c7d
fenris22 <nglaboutit@gmail.com>
2022-11-17 16:49:07 -0600

    figured it out

--- fenc.h
+++ fenc.h
@@ -5,22 +5,10 @@
 #ifndef FENC_FENC_H
 #define FENC_FENC_H
 
+char testing(char t) {
+    return t;
+}
 
-class fenc {
-    mutable table[] = {
-            [0, 'a'],[1, 'b'],[2, 'c'],[3, 'd'],[4, 'e'],
-            [5, 'f'],[6, 'g'],[7, 'h'],[8, 'i'],[9, 'j'],
-            [10, 'k'],[11, 'l'],[12, 'm']
-    }
-
-public: char test(char t) {
-        return t;
-    }
-
-
-
-
-};
-
+char myChar = 't';
 
 #endif //FENC_FENC_H
