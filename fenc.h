cc1549dcd0829d13f4e7d5177d20b5413a06b0e9
fenris <fenristhefox@gmail.com>
2022-12-08 14:09:38 -0600

    attempt 2

---
+++ fenc.h
@@ -0,0 +1,18 @@
+//
+// Created by fenris on 12/8/22.
+//
+
+#ifndef FENC_FENC_H
+#define FENC_FENC_H
+
+// Mode: 0 = encryption, 1 = decryption;
+int fenc (int mode) {
+
+
+
+
+    return mode;
+}
+
+
+#endif //FENC_FENC_H
